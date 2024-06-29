2dfcf7e8075a35fcf22ae72b59065c0fc800c0a6
Lsnsh <zehexin6@gmail.com>
2024-06-29 19:10:04 +0900

    feat(prepare wip): split local text and invoke embeddings model api

--- routes/prepare.js
+++ routes/prepare.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const { TextLoader } = require('langchain/document_loaders/fs/text');
+const { RecursiveCharacterTextSplitter } = require('langchain/text_splitter');
+const {
+  AlibabaTongyiEmbeddings,
+} = require('@langchain/community/embeddings/alibaba_tongyi');
+
+// TODO: switch to POST method
 /* GET prepare(generate vector db static file) listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a status');
+router.get('/', async function (req, res, next) {
+  // TODO: use input from request
+  const loader = new TextLoader('logs/su.txt');
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 20,
+    chunkOverlap: 10,
+  });
+
+  const splitDocs = await splitter.splitDocuments(docs);
+
+  console.log(splitDocs);
+
+  // TODO: may switch to use google embeddings
+  const embeddings = new AlibabaTongyiEmbeddings({});
+  const embeddingsRes = await embeddings.embedQuery(splitDocs[0].pageContent);
+
+  console.log(embeddingsRes);
+
+  // TODO: response json instead of text
+  res.send('ok');
 });
 
 module.exports = router;
